8114233d1428829df175c974da0e78f1cfad7b6c
astriddahlen <astriddahlen@hotmail.com>
2021-12-03 12:21:24 +0100

    updated error message in auth

--- src/com/company/Authentication.java
+++ src/com/company/Authentication.java
@@ -4,10 +4,15 @@ public class Authentication {
      static boolean luhn_validering(String personNumber) {
 
            int insertNumber = personNumber.length() - 10;
+
            if (insertNumber < 0)
            {
-               throw new IllegalArgumentException("Insert 10 numbers!");
+
+               System.out.println("Insert 10 numbers!");
+               new Register();
+
            }
+
         personNumber = personNumber.substring(insertNumber, 10 + insertNumber);
         int sum = 0;
         for (int i = 0; i < personNumber.length(); i++)
